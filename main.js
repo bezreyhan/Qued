e2d4823d8cc134c107b78a5d35a4ace91aac2889
bezreyhan <bezreyhan@gmail.com>
2014-01-18 20:55:00 -0800

    Change colors, fonts, and sizes

--- main.js
+++ main.js
@@ -2,7 +2,29 @@ function appCtrl($scope) {
 	$scope.list = [{name:"Bez", topic:"rails"}];
 
 	$scope.addItem = function() {
-		$scope.list.push({name:$scope.nameInput, topic:$scope.topicInput});
-		// console.log($scope.list[1]);
+		$scope.list.push({name:$scope.nameInput, topic:$scope.topicInput, numInt:1});
+		$scope.nameInput = "";
+		$scope.topicInput = "";
 	};
+
+	$scope.increment = function(item) {
+		item.numInt = item.numInt + 1;
+	};
+
+	$scope.remove = function(item) {
+		var itemIndex = $scope.list.indexOf(item);
+		$scope.list.splice(itemIndex,1);
+	};
+}
+
+function enlarge(span) {
+	span.style.fontSize ="16px";
+	var plus = span.getElementsByClassName("plusOne")[0];
+	plus.style.fontSize = "21px";
+}
+
+function normal(span) {
+	span.style.fontSize ="14px";
+	var plus = span.getElementsByClassName("plusOne")[0];
+	plus.style.fontSize = "18px";
 }
